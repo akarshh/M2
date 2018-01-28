212511ab4deedbbee7cb9fc520625b55b8f2af44
Fe Mello <fernandomello04@hotmail.com>
2018-01-28 16:17:41 -0500

    Person4 implemented

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Fernando
  *  @version 1.1
  */
 public class Person4 {
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        String output = "";
+        for (int i = 0; i < input.length(); i++) {
+            output += (char) (input.charAt(i) + 1);
+        }
+        return output;
     }
     
     /**
@@ -46,5 +50,9 @@ public class Person4 {
       return name + calc(input);
     }
 
+    public static void main(String[] args) {
+        Person4 fe = new Person4("abc123");
+        System.out.println(fe.toString("def456"));
+    }
 }
 
