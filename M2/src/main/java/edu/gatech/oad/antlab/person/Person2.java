5a44de786576222627d19891c8bc95bb25e8981f
rpontes3 <rpontes3@gatech.edu>
2018-01-28 18:08:12 -0500

    fixed random import

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,11 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,7 +33,7 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  java.util.Random rnd = new Random();
+	  Random rnd = new Random();
 	  String out = "";
 	  while (!input.isEmpty()) {
 	  	int i = rnd.nextInt(input.length());
@@ -46,7 +48,7 @@ public class Person2 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
